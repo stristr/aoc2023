625bd45175827668f34c6828d5c52bc2149c4958
Sasha Joseph <sjoseph@netflix.com>
2023-12-10 00:18:21 -0800

    Day 9 revisited

--- 09.py
+++ 09.py
@@ -3,14 +3,8 @@
 from sys import stdin
 
 data = [[int(x) for x in line.split()] for line in stdin.read().strip().split('\n')]
-a, b = 0, 0
-for seq in data:
-    seqs = [seq]
-    while not all(x == 0 for x in seqs[-1]):
-        seqs += [[seqs[-1][i] - seqs[-1][i - 1] for i in range(1, len(seqs[-1]))]]
-    for i in range(len(seqs) - 2, -1, -1):
-        seqs[i] = [seqs[i][0] - seqs[i+1][0]] + seqs[i] + [seqs[i+1][-1] + seqs[i][-1]]
-    a += seqs[0][-1]; b += seqs[0][0]
-
-print('a', a)
-print('b', b)
+windowed = lambda vals, n: list(zip(*[vals[i:] for i in range(n)]))
+a = lambda vals: 0 if all(x == 0 for x in vals) else vals[-1] + a([r - l for l, r in windowed(vals, 2)])
+b = lambda vals: 0 if all(x == 0 for x in vals) else vals[0] + b([r - l for l, r in windowed(vals, 2)])
+print('a', sum(a(vals) for vals in data))
+print('b', sum(b(vals) for vals in data))
